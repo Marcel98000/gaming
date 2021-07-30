a71b0c070be702dc1765d47eb071199b3f95e912
Marcel98000 <77303857+Marcel98000@users.noreply.github.com>
2021-07-30 14:41:48 +0200

    Update title_mask_and_gayming.js

--- js/title_mask_and_gayming.js
+++ js/title_mask_and_gayming.js
@@ -1,10 +1,13 @@
 window.onload = function () {
     // Cooler Titel
-    var title_images = [
-        'img/title1.png',
-        'img/title2.png',
-        'img/title3.png',
-        'img/title4.png'
+    const title_images = [
+        'img/title/1.png',
+        'img/title/2.png',
+        //'img/title/3.png', Diese Beiden sind einfach schlecht, werde ich aber vielleicht überarbeiten
+        //'img/title/4.png',
+        'img/title/5.png',
+        'img/title/6.png',
+        'img/title/7.png'
     ];
     document.querySelector('.mask_title').style.backgroundImage = `url(${title_images[Math.floor(Math.random() * title_images.length)]})`;
     // Gaymer Month
