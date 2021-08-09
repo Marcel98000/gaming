1d1f086a4900e1eb5c849ca8688b412f81c66e7e
Marcel98000 <77303857+Marcel98000@users.noreply.github.com>
2021-08-09 21:51:20 +0200

    Update cookie.js

--- js/cookie.js
+++ js/cookie.js
@@ -1,5 +1,3 @@
-// cookies coming soon:tm: (hab ich noch nicht mal getestet)
-
 window.onload = function () {
     // Loads Cookie Data
     if (document.cookie.indexOf('cookie=') == -1) {
@@ -7,39 +5,35 @@ window.onload = function () {
         expiry_date.setMonth(expiry_date.getMonth() + 1);
         document.cookie = `cookie=item_cookie; expires=${expiry_date.toUTCString()}; path=/`;
     } else {
-        var data = document.cookie.split('; ');
-        for (var i = 0; i < data.length; i++) data[i] = data[i].split('=')[1];
-        var nbt = {
-            id: data[1],
-            count: data[2],
-            name: data[3],
-            lore: data[4],
-            attributes: data[5],
-            enchantments: data[6]
-        };
-        document.getElementById('input_item_id').value = nbt.id;
-        document.getElementById('input_item_count').value = nbt.count;
-        document.getElementById('input_item_name').value = nbt.name;
-        document.getElementById('input_item_lore').value = nbt.lore;
+        var data = JSON.parse(document.cookie.split('=')[1]);
+        document.getElementById('input_item_id').value = data.nbt.id;
+        document.getElementById('input_item_count').value = data.nbt.count;
+        document.getElementById('input_item_name').value = data.nbt.name;
+        document.getElementById('input_item_lore').value = data.nbt.lore;
         // Fügt ein neues Textfeld für jeden Attribut/jede Verzauberung hinzu
         function new_object (type, content) {
-            var object = document.createElement('div');
-            object.innerHTML = `<textarea type = "text" id = "input_item_${type}${document.getElementsByName(`input_item_${type}`).length}" name = "input_item_${type}" value = "${content}">`;
+            var object = document.createElement('div'), i = document.getElementsByName(`input_item_${type}`).length;
+            object.innerHTML = `<textarea type = "text" id = "input_item_${type}${i}" name = "input_item_${type}">`;
             document.body.insertBefore(object, document.getElementById(`new_${type}`));
+            document.getElementById(`input_item_${type}${i}`).value = content;
         }
-        var attributes = nbt.attributes.split('&');
-        attributes.forEach(element => new_object('attribute', element));
-        var enchantments = nbt.enchantments.split('&');
-        enchantments.forEach(element => new_object('enchantment', element));
+        if (data.nbt.attributes.length) {
+            var attributes = data.nbt.attributes.split('&');
+            attributes.forEach(element => new_object('attribute', element));
+        }
+        if (data.nbt.enchantments.length) {
+            var enchantments = data.nbt.enchantments.split('&');
+            enchantments.forEach(element => new_object('enchantment', element));
+        }
     }
 }
 
-window.setInterval(function () {
+window.onbeforeunload = function () {
     // Wandelt die NodeList Werte in einen String um
     function getValues (elements) {
         var string = '';
         elements.forEach(element => string += `${element.value}&`);
-        string.slice(0, -1);
+        string = string.slice(0, -1);
         return string;
     }
     // Saves Data to Cookie every 10 Seconds
@@ -54,5 +48,8 @@ window.setInterval(function () {
     };
     var expiry_date = new Date();
     expiry_date.setMonth(expiry_date.getMonth() + 1);
-    document.cookie = `cookie=item_cookie; id=${nbt.id}; count=${nbt.count}; name=${nbt.name}; lore=${nbt.lore}; attributes=${nbt.attributes}; enchantments=${nbt.enchantments}; expires=${expiry_date.toUTCString()}; path=/`;
-}, 10000);
+    document.cookie = `cookie=${JSON.stringify({
+        cookie_name: 'item_cookie',
+        nbt: nbt
+    })}; expires=${expiry_date.toUTCString()}; path=/`;
+}
